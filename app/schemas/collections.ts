15460212cdec4364d76e8452b88ebda41049316d
edgesoft <wicket.programmer@gmail.com>
2021-11-05 16:27:36 +0100

    add collections for mongo

---
+++ app/schemas/collections.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const collectionSchema = new Schema({
+  headline:  String,
+  shortDescription: String,
+  longDescription:   String,
+  image: String,
+  instagram: String,
+  shortUrl: String
+
+},
+{ collection: 'collections' });
+
+export const Collections = mongoose.models.Collections || mongoose.model('Collections', collectionSchema);
