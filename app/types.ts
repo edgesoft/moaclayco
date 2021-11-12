6a152abd5a81d2fdc09cfb71d22b7f645393a79c
edgesoft <wicket.programmer@gmail.com>
2021-11-12 10:33:14 +0100

    add order type

--- app/types.ts
+++ app/types.ts
@@ -2,3 +2,16 @@ export type OrderItem = {
     itemRef: string
     quantity: number
 }
+
+
+export type Order = {
+    _id: string
+    customer: {
+        firstname: string
+        lastname: string
+        email: string
+
+    }
+    
+}
+
