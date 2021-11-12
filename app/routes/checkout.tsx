f2fcc0501fbbf9cf6cebedb84c02239bec89caca
edgesoft <wicket.programmer@gmail.com>
2021-11-12 10:32:53 +0100

    refactory checkout

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -1,4 +1,9 @@
-import {MetaFunction, LoaderFunction, ActionFunction, useTransition} from 'remix'
+import {
+  MetaFunction,
+  LoaderFunction,
+  ActionFunction,
+  useTransition,
+} from 'remix'
 import {useLoaderData} from 'remix'
 
 import {
@@ -8,10 +13,17 @@ import {
   useStripe,
 } from '@stripe/react-stripe-js'
 import stripeClient from '~/stripeClient'
-import {loadStripe, Stripe, StripeElementLocale, StripePaymentElement} from '@stripe/stripe-js'
+import {
+  loadStripe,
+  Stripe,
+  StripeElementLocale,
+  StripePaymentElement,
+} from '@stripe/stripe-js'
 import {Orders} from '~/schemas/orders'
-import {useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import {classNames} from '~/utils/classnames'
+import Loader from '~/components/loader'
+import Terms from '~/components/terms'
 
 declare global {
   interface Window {
@@ -76,16 +88,16 @@ export let meta: MetaFunction = () => {
   }
 }
 
-
 interface Props {
-  setShow: (show: boolean) => void;
+  setShow: (show: boolean) => void
 }
 
 export default function Index() {
   let data = useLoaderData()
-  let transition = useTransition();
+  let transition = useTransition()
   const [show, setShow] = useState(false)
-  const locale: StripeElementLocale = "sv"
+  const locale: StripeElementLocale = 'sv'
+
   const options = {
     clientSecret: data.clientSecret as string,
     locale,
@@ -94,6 +106,17 @@ export default function Index() {
   const CheckoutForm = ({setShow}: Props) => {
     const stripe = useStripe()
     const elements = useElements()
+    const termsRef: React.RefObject<HTMLInputElement> = useRef(null)
+    const [error, showError] = useState<string | undefined>(undefined)
+    const [showTerm, setShowTerm] = useState<boolean>(false)
+
+    useEffect(() => {
+      if (error) {
+        setTimeout(() => {
+          showError(undefined)
+        }, 2000)
+      }
+    }, [error])
 
     const handleSubmit = async (event: React.FormEvent) => {
       event.preventDefault()
@@ -101,33 +124,68 @@ export default function Index() {
       if (!stripe || !elements) {
         return
       }
+      const agreeTerms = termsRef.current && termsRef.current.checked
+      if (agreeTerms) {
+        const result = await stripe.confirmPayment({
+          elements,
+          confirmParams: {
+            return_url: `${location.protocol}//${location.host}/order`,
+          },
+        })
 
-      const result = await stripe.confirmPayment({
-        elements,
-        confirmParams: {
-          return_url: `${location.protocol}//${location.host}/order`,
-        },
-      })
-
-      if (result.error) {
-        // TODO: handle error
-        console.log(result.error.message)
+        if (result.error) {
+          showError(result.error.message)
+        }
+      } else {
+        showError('Du måste godkänna villkoren')
       }
     }
 
     return (
       <form onSubmit={handleSubmit}>
+        {showTerm ? <Terms show={setShowTerm}/> : null}
         <PaymentElement
           onReady={(e: StripePaymentElement) => {
             setShow(true)
           }}
         />
+        <div className="flex mt-3">
+          <input
+            className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+            ref={termsRef}
+            type="checkbox"
+          />{' '}
+          <span className="-mt-2 px-1 py-1 underline" onClick={() => {
+            setShowTerm(true)
+          }}>
+            Jag godkänner villkoren
+          </span>
+        </div>
         <button
           disabled={!stripe}
-          className="mt-2 p-2 text-white bg-blue-600 rounded"
+          className="mt-2 p-2 text-gray-700 font-medium bg-rosa rounded"
         >
           Gå vidare till betalning
         </button>
+        {error ? (
+          <div className="fixed z-10 bottom-2 left-0 w-screen opacity-95">
+            <div className="relative flex m-1 px-4 py-2 text-red-900 bg-red-100 border-t-4 border-red-500 rounded-b shadow-md">
+              <div className="py-1">
+                <svg
+                  className="mr-4 w-6 h-6 text-red-500 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                </svg>
+              </div>
+              <div>
+                <p className="font-bold">Fel i formulär</p>
+                <p className="text-sm">{error}</p>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </form>
     )
   }
@@ -135,35 +193,24 @@ export default function Index() {
   return (
     <Elements stripe={stripePromise} options={options}>
       <>
-      {show || transition.state === "loading" ? null:  <div className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div
-        className="
-          bg-blue-300
-          animate-ping
-          rounded-full
-          h-32
-          w-32
-          ring-blue-800
-           ring-2 border-blue-500
-        "
-      ></div>
-    </div> }
-      <section
-        className={
-          classNames(
-          show && transition.state !== "loading" ? 'mx-auto px-4 py-5 max-w-6xl sm:px-6 lg:px-4 visible' : 'hidden')
-        }
-      >
-        <div className="grid gap-6 grid-cols-1 my-20 lg:grid-cols-2">
-          <div className="flex flex-col w-full bg-gray-50 rounded-lg shadow-lg">
-            <div className="p-2">
-              <div className="mb-1 text-gray-700 text-xl">Betalning</div>
-              <CheckoutForm setShow={setShow}/>
+     
+        {!show || transition.state === 'loading' ? <Loader /> : null}
+        <section
+          className={classNames(
+            show && transition.state !== 'loading'
+              ? 'mx-auto px-4 py-5 max-w-6xl sm:px-6 lg:px-4 visible'
+              : 'hidden',
+          )}
+        >
+          <div className="grid gap-6 grid-cols-1 my-20 lg:grid-cols-2">
+            <div className="flex flex-col w-full bg-gray-50 rounded-lg shadow-lg">
+              <div className="p-2">
+                <div className="mb-1 text-gray-700 text-xl">Betalning</div>
+                <CheckoutForm setShow={setShow} />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-   
+        </section>
       </>
     </Elements>
   )
