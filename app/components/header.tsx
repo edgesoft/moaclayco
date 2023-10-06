be47c1a80e5c3acdaa9f67267d94fc3f6e2cf055
edgesoft <wicket.programmer@gmail.com>
2023-10-06 20:28:25 +0200

    Fix menu
    fix: #56

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -70,6 +70,31 @@ function Hamburger() {
                     </svg>
                   </div>
                 </div>
+                <div
+                  className="flex m-2 w-full cursor-pointer"
+                  onClick={() => {
+                    history(`/`);
+                    setMenu(false);
+                  }}
+                >
+                  <div className="flex-shrink-0 w-12 h-12 md:w-18 md:h-18">
+                    <svg
+                      className="h-10 w-10"
+                      viewBox="0 0 20 20"
+                      fill="#F4B9A4"
+                    >
+                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                    </svg>
+                  </div>
+                  <span className="m-3 md:my-3">Startsida</span>
+                </div>
+
+                <div className="flex m-2 w-full">
+                  <div
+                    style={{ width: 255 }}
+                    className="flex border-gray-600 border-b-2 px-2"
+                  ></div>
+                </div>
                 {data &&
                   data.collections.map((d: CollectionProps, i: number) => {
                     return (
