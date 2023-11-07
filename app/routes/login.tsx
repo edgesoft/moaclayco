f5c981675fa49dc41ee496cb0c0318584ba7bbef
edgesoft <wicket.programmer@gmail.com>
2023-11-07 19:08:44 +0100

    set type submit

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -48,10 +48,7 @@ export default function Login() {
           </div>
 
           <div className="mt-4">
-            <button
-              onClick={() => {}}
-              className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring-blue active:bg-blue-700 transition duration-150 ease-in-out"
-            >
+            <button type="submit" className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring-blue active:bg-blue-700 transition duration-150 ease-in-out">
               {magicLinkSent ? `Länk skickad` : `Skicka länk`}
             </button>
           </div>
