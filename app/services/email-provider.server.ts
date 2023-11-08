6f11a6425e2d194f845cddca66bd6cf6d9dc7295
edgesoft <wicket.programmer@gmail.com>
2023-11-08 13:22:20 +0100

    Logging for email

--- app/services/email-provider.server.ts
+++ app/services/email-provider.server.ts
@@ -1,28 +1,32 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
- type SendMail = {
-   emailAddress: string,
-   subject: string,
-   body: string
- }
+type SendMail = {
+  emailAddress: string;
+  subject: string;
+  body: string;
+};
 
-  export const sendEmail = async ({emailAddress, subject, body}:  SendMail) => {
-    let transporter = nodemailer.createTransport({
-      host: 'send.one.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    })
-  
+export const sendEmail = async ({ emailAddress, subject, body }: SendMail) => {
+  let transporter = nodemailer.createTransport({
+    host: "send.one.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  try {
     let info = await transporter.sendMail({
-      from: 'support@moaclayco.com',
+      from: "support@moaclayco.com",
       to: emailAddress,
       bcc: "wicket.programmer@gmail.com",
       subject,
       html: body,
-    })
-    console.log(info)
+    });
+    console.log(info);
+  } catch (e) {
+    console.log(e);
   }
+};
