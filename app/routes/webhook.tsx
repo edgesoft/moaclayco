6d9ea85e9684e8f64f9c47c8f91aaedc813383e1
edgesoft <wicket.programmer@gmail.com>
2023-11-10 04:47:13 +0100

    change webhook to use html mail instead

--- app/routes/webhook.tsx
+++ app/routes/webhook.tsx
@@ -1,83 +1,79 @@
-import mongoose from 'mongoose'
-import { ActionFunction, LoaderFunction } from '@remix-run/node'
-import { Stripe } from 'stripe'
-import { Items } from '~/schemas/items'
-import { Orders } from '~/schemas/orders'
-import { OrderItem, Order} from '~/types'
-import nodemailer from 'nodemailer'
-import { Discounts } from '~/schemas/discounts'
+import mongoose from "mongoose";
+import { ActionFunction } from "@remix-run/node";
+import { Stripe } from "stripe";
+import { Items } from "~/schemas/items";
+import { Orders } from "~/schemas/orders";
+import { OrderItem, Order } from "~/types";
+import { Discounts } from "~/schemas/discounts";
+import EmailOrderTemplate, { Template } from "~/components/mail/order";
+import { renderToString } from "react-dom/server";
+import { transporter } from "~/services/email-provider.server";
 
+export const sendMail = async (order: Order, template: Template) => {
+  try {
+    let info = await transporter.sendMail({
+      from: "support@moaclayco.com",
+      to: order.customer.email,
+      bcc: "moaclayco@gmail.com,wicket.programmer@gmail.com,support@moaclayco.com",
+      subject: template === Template.ORDER ? `Order ${order._id} (moaclayco.com)` :  `Din order ${order._id} är nu påväg!`,
+      html: renderToString(<EmailOrderTemplate order={order} template={template} />),
+    });
 
-const sendMail = async (order: Order) => {
-  let transporter = nodemailer.createTransport({
-    host: 'send.one.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  })
-
-  let info = await transporter.sendMail({
-    from: 'support@moaclayco.com',
-    to: order.customer.email,
-    bcc: "moaclayco@gmail.com,wicket.programmer@gmail.com,support@moaclayco.com",
-    subject: `Order ${order._id} (moaclayco.com)`,
-    text: `Hej ${order.customer.firstname} ${order.customer.lastname}!\nTack för din order (${order._id})\n\nVi kommer att behandla ordern så snart vi kan.\n\nMed vänliga hälsningar Moa Clay Collection`,
-  })
-
-  console.log('Message sent: %s', info.messageId)
-}
-
-
+    console.log("Message sent: %s", info.messageId);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const fromPaymentIntent = async (id: string, status: string) => {
-
   const order: Order | null = await Orders.findOne({
-    'paymentIntent.id': id,
-  })
+    "paymentIntent.id": id,
+  }).lean();
 
   if (order) {
-    await Orders.updateOne({_id: order._id}, {status, webhookAt: new Date()})
+    await Orders.updateOne(
+      { _id: order._id },
+      { status, webhookAt: new Date() }
+    );
     if (order.discount && order.discount.amount > 0) {
-      await Discounts.updateOne({code: order.discount.code}, {used: true, usedAt: new Date()})
+      await Discounts.updateOne(
+        { code: order.discount.code },
+        { used: true, usedAt: new Date() }
+      );
     }
-    if (status === 'SUCCESS') {
+    if (status === "SUCCESS") {
       order.items.map(async (i: OrderItem) => {
         await Items.updateOne(
-          {_id: new mongoose.Types.ObjectId(i.itemRef)},
-          {$inc: {amount: -i.quantity}},
-        )
-      })
-      await sendMail(order)
+          { _id: new mongoose.Types.ObjectId(i.itemRef) },
+          { $inc: { amount: -i.quantity } }
+        );
+      });
+      await sendMail(order, Template.ORDER);
     }
   } else {
-    console.log("Could not find order")
+    console.log("Could not find order");
   }
-}
+};
 
-export let action: ActionFunction = async ({request}) => {
-
-  const sig = request.headers.get('Stripe-Signature')
-  let body = await request.text()
+export let action: ActionFunction = async ({ request }) => {
+  const sig = request.headers.get("Stripe-Signature");
+  let body = await request.text();
 
   try {
-    let event = JSON.parse(body)
-    let paymentIntent = event.data.object as Stripe.PaymentIntent
+    let event = JSON.parse(body);
+    let paymentIntent = event.data.object as Stripe.PaymentIntent;
     switch (event.type) {
-      case 'payment_intent.succeeded':
-        await fromPaymentIntent(paymentIntent.id, 'SUCCESS')
-        break
-      case 'payment_intent.canceled':
-        await fromPaymentIntent(paymentIntent.id, 'CANCELED')
-        break
-      case 'payment_intent.payment_failed':
-        await fromPaymentIntent(paymentIntent.id, 'FAILED')
-        break
+      case "payment_intent.succeeded":
+        await fromPaymentIntent(paymentIntent.id, "SUCCESS");
+        break;
+      case "payment_intent.canceled":
+        await fromPaymentIntent(paymentIntent.id, "CANCELED");
+        break;
+      case "payment_intent.payment_failed":
+        await fromPaymentIntent(paymentIntent.id, "FAILED");
+        break;
     }
   } catch (e) {}
 
-  return new Response().ok
-}
-
+  return new Response().ok;
+};
