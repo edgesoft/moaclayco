3e0864ea0f1b845fa2b3786a29a1d9ada74efc9d
edgesoft <wicket.programmer@gmail.com>
2023-11-10 04:47:41 +0100

    export transporter

--- app/services/email-provider.server.ts
+++ app/services/email-provider.server.ts
@@ -6,26 +6,25 @@ type SendMail = {
   body: string;
 };
 
-export const sendEmail = async ({ emailAddress, subject, body }: SendMail) => {
-  let transporter = nodemailer.createTransport({
-    host: "send.one.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+export const transporter = nodemailer.createTransport({
+  host: "send.one.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
+export const sendEmail = async ({ emailAddress, subject, body }: SendMail) => {
   try {
-    let info = await transporter.sendMail({
+    await transporter.sendMail({
       from: "support@moaclayco.com",
       to: emailAddress,
       bcc: "wicket.programmer@gmail.com",
       subject,
       html: body,
     });
-    console.log(info);
   } catch (e) {
     console.log(e);
   }
