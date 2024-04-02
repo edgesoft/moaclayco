ee34d37a29d2e8099bb376d4c2bd9bd244bea5d7
edgesoft <wicket.programmer@gmail.com>
2024-04-02 22:40:09 +0200

    add types

--- app/types.ts
+++ app/types.ts
@@ -1,90 +1,96 @@
 export type OrderItem = {
-     _id: string
-    itemRef: string
-    name: string
-    image: string
-    quantity: number
-    price: number
-    additionalItems: [
-      {
-        name: string
-        price: number
-        packinfo: string
-      }
-    ]
-  }
-  
-  export type Order = {
-    _id: string
-    totalSum: number
-    paymentIntent?: {
-      id: string
-      client_secret: string
+  _id: string;
+  itemRef: string;
+  name: string;
+  image: string;
+  quantity: number;
+  price: number;
+  additionalItems: [
+    {
+      name: string;
+      price: number;
+      packinfo: string;
     }
-    customer: {
-      firstname: string
-      lastname: string
-      email: string
-    }
-    discount: {
-      amount: number
-      percentage: number | undefined
-      code: string | undefined
-    }
-    freightCost: number
-    items: [OrderItem]
-  }
-  
-  export type CollectionProps = {
-    _id?: string
-    image: string
-    headline: string
-    longDescription: string
-    shortDescription: string
-    instagram?: string
-    twitter?: string
-    shortUrl: string
-    index?: number
-  }
-  
+  ];
+};
+
+export type Order = {
+  _id: string;
+  totalSum: number;
+  paymentIntent?: {
+    id: string;
+    client_secret: string;
+  };
+  customer: {
+    firstname: string;
+    lastname: string;
+    email: string;
+  };
+  discount: {
+    amount: number;
+    percentage: number | undefined;
+    code: string | undefined;
+  };
+  freightCost: number;
+  items: [OrderItem];
+};
+
+export type CollectionProps = {
+  _id?: string;
+  image: string;
+  headline: string;
+  longDescription: string;
+  shortDescription: string;
+  instagram?: string;
+  twitter?: string;
+  shortUrl: string;
+  index?: number;
+};
+
 export type AdditionalItem = {
-  price: number
-  name: string
-}
+  price: number;
+  name: string;
+};
 
-  export type ItemProps = {
-    _id: string
-    images: string[]
-    amount: number
-    price: number
-    headline: string
-    collectionRef: string
-    productInfos?: string[]
-    additionalItems?: AdditionalItem[]
-    instagram?: string
-    longDescription?: string,
-  }
+export type ItemProps = {
+  _id: string;
+  images: string[];
+  amount: number;
+  price: number;
+  headline: string;
+  collectionRef: string;
+  productInfos?: string[];
+  additionalItems?: AdditionalItem[];
+  instagram?: string;
+  longDescription?: string;
+};
 
+export type User = {
+  firstname: string;
+  lastname: string;
+  email: string;
+};
 
-  export type User = {
-    firstname: string
-    lastname: string
-    email: string
-  }
+export type AdditionalItemProps = {
+  item: AdditionalItem;
+  handleSwitch: (
+    item: AdditionalItem,
+    on: boolean,
+    additionalIndex: number
+  ) => void;
+  additionalIndex: number;
+};
 
-  export type AdditionalItemProps = {
-    item: AdditionalItem;
-    handleSwitch: (
-      item: AdditionalItem,
-      on: boolean,
-      additionalIndex: number
-    ) => void;
-    additionalIndex: number;
-  };
-  
-  export type AdditionCartItemType = {
-    item: AdditionalItem;
-    index: number;
-    additionalIndex: number;
-  };
-  
+export type AdditionCartItemType = {
+  item: AdditionalItem;
+  index: number;
+  additionalIndex: number;
+};
+
+export type DiscountType = {
+  _id: string;
+  percentage: number;
+  code: string;
+  balance: number;
+  expireAt: Date | null;
+};
