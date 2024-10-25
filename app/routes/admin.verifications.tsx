63ecf0f23e69eccd6db248ac547b99315ddd0e54
edgesoft <wicket.programmer@gmail.com>
2024-10-25 22:48:45 +0200

    add accouts

--- app/routes/admin.verifications.tsx
+++ app/routes/admin.verifications.tsx
@@ -61,17 +61,21 @@ type FormData = z.infer<typeof formSchema>;
 
 const accounts = [
   { value: 1580, label: "1580 - Fordran på Stripe" },
-  {value: 1510, label: "1510 - Kundfordringar"},
+  { value: 1510, label: "1510 - Kundfordringar"},
+  { value: 1630, label: "1630 - Skattekonto"},
   { value: 1930, label: "1930 - Bank" },
   { value: 2013, label: "2013 - Eget uttag" },
   { value: 2018, label: "2018 - Egen insättning" },
   { value: 2611, label: "2611 - Utgående moms på varor och frakt" },
   { value: 2640, label: "2640 - Ingående moms" },
+  { value: 2650, label: "2650 - Momsskuld" },
   { value: 3001, label: "3001 - Försäljning av varor", vatAccount: 2611 }, // Moms krävs
   { value: 3740, label: "3740 - Öres- och kronutjämning" },
   { value: 4000, label: "4000 - Material/Varukostnader", vatAccount: 2640 }, // Moms krävs
   { value: 6570, label: "6570 - Kostnader för betalningsförmedling" },
-];
+  { value: 8313, label: "8313 - Ränteintäkter bank" },
+  
+]
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
