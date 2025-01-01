900ffc2b1aa0cbf2d7f1e4b679bfafe58691c6fe
edgesoft <wicket.programmer@gmail.com>
2025-01-01 22:01:38 +0100

    get fiscalYear in verifications

--- app/routes/admin.verifications.tsx
+++ app/routes/admin.verifications.tsx
@@ -4,19 +4,32 @@ import { Verifications } from "~/schemas/verifications";
 import { ListVerification } from "~/components/admin/listVerification";
 import { VerificationProps } from "~/types";
 import { getDomain } from "~/utils/domain";
+import { auth } from "~/services/auth.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const domain =  getDomain(request);
-  const year = new Date().getFullYear()
 
-  // Kör båda asynkrona anropen parallellt med Promise.all()
+  const user = await auth.isAuthenticated(request, { failureRedirect: "/login" });  
+  const domain =  getDomain(request);
+
+
   const verificationsPromise = Verifications.find({
     verificationDate: {
-      $gte: new Date(`${year}-01-01`),
-      $lt: new Date(`${year + 1}-01-01`),
+      $gte: new Date(`${user.fiscalYear}-01-01`),
+      $lt: new Date(`${user.fiscalYear + 1}-01-01`),
     },
     domain: domain?.domain,
   }).sort({ verificationDate: -1 });
+
+  const vatReportsPromise = Verifications.find({
+    "metadata.key": "vatReport", // Endast momsrapporter
+    verificationDate: {
+      $gte: new Date(`${user.fiscalYear + 1}-01-01`), // Start januari året efter
+      $lt: new Date(`${user.fiscalYear + 1}-03-01`), // Slut februari året efter
+    },
+    domain: domain?.domain,
+  }).exec()
+
+
 
   // Hämta senaste verifikationsnumret
   const latestVerificationNumberPromise = Verifications.findOne({domain: domain?.domain})
@@ -24,15 +37,16 @@ export const loader: LoaderFunction = async ({ request }) => {
     .select("verificationNumber") // Hämta bara verifikationsnumret
     .exec();
 
-  const [verifications, latestVerification] = await Promise.all([
+  const [verifications, latestVerification, vatReports] = await Promise.all([
     verificationsPromise,
     latestVerificationNumberPromise,
+    vatReportsPromise
   ]);
 
   // Extrahera verifikationsnumret om det finns ett resultat
   const latestVerificationNumber = latestVerification?.verificationNumber || 0;
 
-  return json({ verifications, year, latestVerificationNumber });
+  return json({ verifications, year: user.fiscalYear, latestVerificationNumber, vatReports });
 };
 
 const groupByMonth = (verifications: VerificationProps[]) => {
@@ -68,27 +82,39 @@ const groupByMonth = (verifications: VerificationProps[]) => {
 
 type LoaderData = {
   verifications: VerificationProps[];
+  vatReports: VerificationProps[];
   latestVerificationNumber: number;
   year: number;
 };
 
 export default function VerificationsPage() {
-  const { verifications, year, latestVerificationNumber } =
+  const { verifications, year, latestVerificationNumber, vatReports } =
     useLoaderData<LoaderData>();
   const groupedVerifications = groupByMonth(verifications);
 
+const hasVatReport = (
+  verifications: VerificationProps[],
+  monthKey: string
+): VerificationProps | undefined => {
+  const foundInVerifications = verifications.find((verification) =>
+    verification.metadata.some(
+      (meta) => meta.key === "vatReport" && meta.value === monthKey
+    )
+  );
 
+  // Om momsrapport hittas i vanliga verifikationer, returnera den
+  if (foundInVerifications) {
+    return foundInVerifications;
+  }
 
-  const hasVatReport = (
-    verifications: VerificationProps[],
-    monthKey: string
-  ) => {
-    return verifications.find((verification: VerificationProps) =>
-      verification.metadata.some(
-        (meta) => meta.key === "vatReport" && meta.value === monthKey
-      )
-    );
-  };
+  const foundInVatReports = vatReports.find((report) =>
+    report.metadata.some(
+      (meta) => meta.key === "vatReport" && meta.value === monthKey
+    )
+  );
+
+  return foundInVatReports;
+};
 
   return (
     <div className="mt-20 p-2">
@@ -108,6 +134,13 @@ export default function VerificationsPage() {
             className="ml-2 bg-slate-800 text-white px-3 py-1 rounded-lg text-sm"
           >
             Balans och resultaträkning
+          </Link>
+          <Link
+            to="/admin/verifications/settings"
+            prefetch="intent"
+            className="ml-2 bg-slate-800 text-white px-3 py-1 rounded-lg text-sm"
+          >
+            Välj bokföringsår
           </Link>
         </div>
       </div>
