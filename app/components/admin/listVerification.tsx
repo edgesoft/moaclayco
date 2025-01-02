c4217539fd79f4e3b98d9d0093160596f397f8ad
edgesoft <wicket.programmer@gmail.com>
2025-01-02 16:37:36 +0100

    Styling verification list view

--- app/components/admin/listVerification.tsx
+++ app/components/admin/listVerification.tsx
@@ -14,6 +14,17 @@ type GroupVerificationProps = {
   isExpanded: Boolean;
 };
 
+function findMinMax(array) {
+  return array.reduce(
+    (acc, obj) => {
+      acc.min = Math.min(acc.min, obj.verificationNumber);
+      acc.max = Math.max(acc.max, obj.verificationNumber);
+      return acc;
+    },
+    { min: Infinity, max: -Infinity }
+  );
+}
+
 export function ListVerification({
   monthKey,
   groupedVerifications,
@@ -22,6 +33,8 @@ export function ListVerification({
 }: GroupVerificationProps) {
   const navigate = useNavigate();
   const [expanded, setExpanded] = useState(isExpanded);
+
+  const minMax = findMinMax(groupedVerifications[monthKey]);
 
   const shouldRegisterVat = (verification: VerificationProps | undefined) => {
     if (!verification) {
@@ -59,14 +72,20 @@ export function ListVerification({
   };
 
   return (
-    <div key={monthKey} className={classNames(!expanded ? `bg-gray-100`: `bg-white`,"mb-4 border border-gray-200 rounded-md")}>
+    <div
+      key={monthKey}
+      className={classNames(
+        !expanded ? `bg-gray-100` : `bg-white`,
+        "mb-4 border border-gray-200 rounded-md"
+      )}
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setExpanded(!expanded);
+      }}
+    >
       <div className="flex justify-between items-center mb-4 cursor-pointer p-2">
-        <h2
-          onClick={() => {
-            setExpanded(!expanded);
-          }}
-          className="text-xl font-semibold flex items-center justify-between cursor-pointer"
-        >
+        <h2 className="text-xl font-semibold flex items-center justify-between cursor-pointer">
           {formatMonthName(monthKey)}
           <svg
             className={`w-5 h-5 transform transition-transform duration-300 ${
@@ -86,27 +105,62 @@ export function ListVerification({
           </svg>
         </h2>
 
-        {!vatReportVerification && isPastMonth(monthKey) && (
-          <button
-            onClick={() => handleCreateVATReport(monthKey)}
-            className="bg-slate-800 text-white px-3 py-1 rounded-lg text-sm"
-          >
-            Skapa momsrapport ({formatMonthName(monthKey)})
-          </button>
-        )}
+        <div className="flex items-center justify-between">
+          <div className="text-sm text-gray-600 mr-2 flex items-center">
+            <span className="bg-gray-300 text-gray-500 inline-flex px-2 border-b border-gray-500 text-xs font-semibold leading-5 rounded-full">
+              A{minMax.min}
+            </span>
+            {minMax.min < minMax.max && (
+              <div className="flex items-center">
+                <svg
+                  className="w-4 h-4 transform transition-transform duration-300 rotate-360 text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+                <span className="bg-gray-300 text-gray-500 inline-flex px-2 text-xs font-semibold leading-5 border-b border-gray-500 rounded-full">
+                  A{minMax.max}
+                </span>
+              </div>
+            )}
+          </div>
 
-        {vatReportVerification && registerVat && isPastMonth(monthKey) && (
-          <button
-            onClick={() =>
-              navigate(
-                `/admin/verifications/vat-report-payed?month=${monthKey}`
-              )
-            }
-            className="bg-slate-800 text-white px-3 py-1 rounded-lg text-sm"
-          >
-            Registrera/Skattemyndigheten ({formatMonthName(monthKey)})
-          </button>
-        )}
+          {!vatReportVerification && isPastMonth(monthKey) && (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleCreateVATReport(monthKey);
+              }}
+              className="bg-slate-800 text-white px-3 py-1 rounded-lg text-sm"
+            >
+              Skapa momsrapport ({formatMonthName(monthKey)})
+            </button>
+          )}
+
+          {vatReportVerification && registerVat && isPastMonth(monthKey) && (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                navigate(
+                  `/admin/verifications/vat-report-payed?month=${monthKey}`
+                );
+              }}
+              className="bg-slate-800 text-white px-3 py-1 rounded-lg text-sm"
+            >
+              Registrera/Skattemyndigheten ({formatMonthName(monthKey)})
+            </button>
+          )}
+        </div>
       </div>
 
       {expanded ? (
