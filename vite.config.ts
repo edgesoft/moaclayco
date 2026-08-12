0f124f4380b43193d16fab37f9d6f114b99465df
Mathias Nilsson <wicket.programmer@gmail.com>
2026-08-12 18:33:57 +0200

    Upgrade Tailwind CSS to version 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
