a4262f471af89f53450f5a8883ebeadb508d4a21
Mathias Nilsson <wicket.programmer@gmail.com>
2026-08-12 20:26:57 +0200

    Prebundle swipe dependency for client navigation

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ["react-swipeable"],
+  },
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
+    dedupe: ["react", "react-dom"],
     tsconfigPaths: true,
   },
 });
