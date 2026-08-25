296b9c34b9add22226f6e8bf1e69aade8cce74a1
Mathias Nilsson <wicket.programmer@gmail.com>
2026-08-25 21:23:14 +0200

    Let Vite manage shared editor styles
    Import the editor stylesheet through the module graph so route transitions keep consistent styling.

--- app/routes/collections.$collection_.edit.tsx
+++ app/routes/collections.$collection_.edit.tsx
@@ -1,13 +1,9 @@
-import type { LinksFunction } from "react-router";
 import { CollectionAction } from "~/actions/collection";
 import CollectionEditor from "~/components/admin/collection";
 import { CollectionLoader } from "~/loaders/collection";
-import itemEditorStyles from "~/styles/item-editor.css?url";
+import "~/styles/item-editor.css";
 
 export const loader = CollectionLoader;
 export const action = CollectionAction;
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: itemEditorStyles },
-];
 
 export default CollectionEditor;
